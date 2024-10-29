81dfdc4643958972e81e0eed26efe94682469c14
redreev8 <redreevs@mail.ru>
2024-10-29 13:16:23 +0500

    UI: add section

--- src/ui/section/section.tsx
+++ src/ui/section/section.tsx
@@ -2,10 +2,13 @@ import classNames from 'classnames'
 import { AreaHTMLAttributes, forwardRef } from 'react'
 import style from './section.module.scss'
 
-const Section = forwardRef<HTMLElement, AreaHTMLAttributes<HTMLElement>>(function sectionRef({ children, className,...props }, ref) {
+const Section = forwardRef<HTMLElement, AreaHTMLAttributes<HTMLElement>>(function sectionRef(
+    { children, className, ...props },
+    ref
+) {
     return (
-        <section ref={ ref } className={ classNames(style['section'], className) } { ...props }>
-            { children }
+        <section ref={ref} className={classNames(style['section'], className)} {...props}>
+            {children}
         </section>
     )
 })
