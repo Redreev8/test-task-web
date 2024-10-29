85201b7dc2785567171084b0204a7bbc414ca966
redreev8 <redreevs@mail.ru>
2024-10-29 13:14:59 +0500

    UI: add btn

--- src/ui/btn/btn.tsx
+++ src/ui/btn/btn.tsx
@@ -1,6 +1,6 @@
 'use client'
 import style from './btn.module.scss'
-import {forwardRef, ForwardedRef, ButtonHTMLAttributes, AnchorHTMLAttributes} from 'react'
+import { forwardRef, ForwardedRef, ButtonHTMLAttributes, AnchorHTMLAttributes } from 'react'
 import classNames from 'classnames'
 import Link from 'next/link'
 
@@ -12,40 +12,52 @@ export interface Props {
 export type ButtonProps = Props & ButtonHTMLAttributes<HTMLButtonElement>
 export type LinkProps = Props & AnchorHTMLAttributes<HTMLAnchorElement>
 
-const Button = forwardRef<HTMLButtonElement, ButtonProps>(function ButtonRef({children, ...props}, ref) {
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(function ButtonRef(
+    { children, ...props },
+    ref
+) {
     return (
         <button ref={ref} {...props}>
             {children}
         </button>
     )
 })
-const A = forwardRef<HTMLAnchorElement, LinkProps>(function ARef({children, href, ...props}, ref) {
+const A = forwardRef<HTMLAnchorElement, LinkProps>(function ARef(
+    { children, href, ...props },
+    ref
+) {
     return (
-        <Link href={ href! } ref={ref} {...props}>
+        <Link href={href!} ref={ref} {...props}>
             {children}
         </Link>
     )
 })
 
-const Btn = forwardRef<HTMLButtonElement | HTMLAnchorElement, LinkProps | ButtonProps>(function BtnRef(
-    {className, children, href, isCard, ...props},
-    ref
-) {
-    const cl = classNames(style.btn, className, {
-        [style['btn--card']]: isCard,
-    })
-    if (href) {
+const Btn = forwardRef<HTMLButtonElement | HTMLAnchorElement, LinkProps | ButtonProps>(
+    function BtnRef({ className, children, href, isCard, ...props }, ref) {
+        const cl = classNames(style.btn, className, {
+            [style['btn--card']]: isCard
+        })
+        if (href) {
+            return (
+                <A
+                    {...(props as LinkProps)}
+                    href={href}
+                    ref={ref as ForwardedRef<HTMLAnchorElement>}
+                    className={cl}>
+                    {children}
+                </A>
+            )
+        }
         return (
-            <A {...(props as LinkProps)} href={href} ref={ref as ForwardedRef<HTMLAnchorElement>} className={cl}>
+            <Button
+                {...(props as ButtonProps)}
+                ref={ref as ForwardedRef<HTMLButtonElement>}
+                className={cl}>
                 {children}
-            </A>
+            </Button>
         )
     }
-    return (
-        <Button {...(props as ButtonProps)} ref={ref as ForwardedRef<HTMLButtonElement>} className={cl}>
-            {children}
-        </Button>
-    )
-})
+)
 
 export default Btn
