0b249ac9cee64cb08a92db6218aacafc186ed5a6
redreev8 <redreevs@mail.ru>
2024-10-29 13:37:49 +0500

    components: add header && footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,15 +1,13 @@
-import Card from "@/ui/card"
-import Container from "@/ui/container"
+import Card from '@/ui/card'
+import Container from '@/ui/container'
 import style from './footer.module.scss'
 
 const Footer = () => {
     return (
-        <footer className={ style['footer'] }>
+        <footer className={style['footer']}>
             <Container>
                 <Card isTitle>
-                    <span className={ style['copy'] }>
-                        Some Company 2024
-                    </span>
+                    <span className={style['copy']}>Some Company 2024</span>
                 </Card>
             </Container>
         </footer>
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,19 +1,17 @@
-import Btn from "@/ui/btn"
-import Card from "@/ui/card"
-import Container from "@/ui/container"
-import Logo from "@/ui/logo"
+import Btn from '@/ui/btn'
+import Card from '@/ui/card'
+import Container from '@/ui/container'
+import Logo from '@/ui/logo'
 import style from './header.module.scss'
 
 const Header = () => {
     return (
-        <header className={ style['header'] }>
+        <header className={style['header']}>
             <Container>
-                <Card className={ style['wrapp'] }>
-                    <Logo/>
+                <Card className={style['wrapp']}>
+                    <Logo />
                     <nav>
-                        <Btn href="/contacts">
-                            Contact us
-                        </Btn>
+                        <Btn href="/contacts">Contact us</Btn>
                     </nav>
                 </Card>
             </Container>
