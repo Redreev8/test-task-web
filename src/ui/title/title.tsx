3296602389f6b2f273161c96a3977ba541cf4bac
redreev8 <redreevs@mail.ru>
2024-10-29 13:43:30 +0500

    UI: add text && title

---
+++ src/ui/title/title.tsx
@@ -0,0 +1,26 @@
+import {forwardRef, HtmlHTMLAttributes, ReactNode} from 'react'
+import classNames from 'classnames'
+import style from './title.module.scss'
+import listComponentTitle from './list-title'
+export interface TitleProps extends HtmlHTMLAttributes<HTMLHeadingElement> {
+    children: ReactNode
+    level?: number
+    levelTag?: number
+    className?: string
+}
+
+const Title = forwardRef<HTMLHeadingElement, TitleProps>(function TitleRef(
+    {className, level = 2, levelTag = level, children, ...props},
+    ref
+) {
+    const cl = classNames(style.title, className, style[`title--${level}`])
+
+    const Componet = listComponentTitle[levelTag]
+    return (
+        <Componet ref={ref} className={cl} {...props}>
+            {children}
+        </Componet>
+    )
+})
+
+export default Title
