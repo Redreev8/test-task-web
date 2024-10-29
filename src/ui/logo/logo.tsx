94feb5fa2ac99b46c6fed3bfd38e752485af0394
redreev8 <redreevs@mail.ru>
2024-10-29 13:27:34 +0500

    ui: add logo

---
+++ src/ui/logo/logo.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+import style from './logo.module.scss'
+
+const Logo = () => {
+    return (
+        <Link className={ style['logo'] } href='/'>Some Company</Link>
+    )
+}
+
+export default Logo
