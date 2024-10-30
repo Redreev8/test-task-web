3a61fbdfee60a66ffe38c8fee64e9642e419074e
redreev8 <redreevs@mail.ru>
2024-10-30 21:22:34 +0500

    app: add Open Graph tags trwiter

--- src/app/contacts/page.tsx
+++ src/app/contacts/page.tsx
@@ -1,7 +1,23 @@
 import ContactSection from '@/components/contact-section'
+import Head from 'next/head'
 
 const Contacts = () => {
-    return <ContactSection></ContactSection>
+    const title = 'contacts page tacks'
+    const description = 'contacts page written in next js, scss, radix form'
+    const name = 'Test task website contacts Some Company'
+    return (
+        <>
+            <Head>
+                <meta property="og:title" content={title} />
+                <meta property="og:description" content={description} />
+                <meta name="twitter:title" content={title} />
+                <meta name="twitter:description" content={description} />
+                <meta property="og:site_name" content={name} />
+                <meta name="twitter:card" content={name} />
+            </Head>
+            <ContactSection />
+        </>
+    )
 }
 
 export default Contacts
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,22 @@
 import AlsoSection from '@/components/also-section'
 import HomeIntro from '@/components/home-intro'
 import LessSection from '@/components/less-section'
+import Head from 'next/head'
 
 const Home = () => {
+    const title = 'home page tacks'
+    const description = 'home page written in next js, scss, radix form'
+    const name = 'Test task website contacts Some Company'
     return (
         <>
+            <Head>
+                <meta property="og:title" content={title} />
+                <meta property="og:description" content={description} />
+                <meta name="twitter:title" content={title} />
+                <meta name="twitter:description" content={description} />
+                <meta property="og:site_name" content={name} />
+                <meta name="twitter:card" content={name} />
+            </Head>
             <HomeIntro />
             <AlsoSection />
             <LessSection />
@@ -12,4 +24,4 @@ const Home = () => {
     )
 }
 
-export default Home;
+export default Home
