568fe1cf1f55636c7375955ba2293b90331d5100
redreev8 <redreevs@mail.ru>
2024-12-15 14:54:21 +0500

    ui: add svg & img-svg

---
+++ src/ui/svg/index.ts
@@ -0,0 +1,13 @@
+import { AreaHTMLAttributes, ForwardRefExoticComponent, RefAttributes } from 'react'
+import ImgSvg from './img-svg'
+
+interface SvgList {
+    [key: string]: ForwardRefExoticComponent<AreaHTMLAttributes<SVGSVGElement> & RefAttributes<SVGSVGElement>>
+}
+
+const svgList: SvgList = {
+    img: ImgSvg,
+    str: ImgSvg
+}
+
+export default svgList
