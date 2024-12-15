5ac338f3120e85f96813c654051122b506903025
redreev8 <redreevs@mail.ru>
2024-12-15 15:02:25 +0500

    components: add server action also && card also

---
+++ src/components/also-section/get-also.ts
@@ -0,0 +1,11 @@
+import { AlsoCardProps } from "./also-card"
+
+const getAlso: () => Promise<AlsoCardProps[]> = async () => {
+    return Array.from({ length: 6 }, () => ({
+        icon: 'img',
+        title: 'Title',
+        text: ['Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam mattis, leo et condimentum']
+    } as AlsoCardProps))
+}
+
+export default getAlso
